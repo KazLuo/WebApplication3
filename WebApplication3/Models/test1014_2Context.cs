78cd152ce6376f1cc67fe6baf3ce7c6cb35d5a54
Kaz <ccc5211@gmail.com>
2023-10-14 15:17:30 +0800

    建立環境

---
+++ WebApplication3/Models/test1014_2Context.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace WebApplication3.Models
+{
+    public partial class test1014_2Context : DbContext
+    {
+        public test1014_2Context()
+            : base("name=test1014_2")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
